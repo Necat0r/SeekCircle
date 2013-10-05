0ad9f2cc4e96aa9f9ecbc084dc3b6abea2444d21
Necat0r <necat0r@server.fake>
2013-10-05 14:20:42 +0200

    Added sample project for SeekCircle

---
+++ sample/src/com/sample/SampleActivity.java
@@ -0,0 +1,51 @@
+package com.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.seekcircle.sample.R;
+import com.seeksircle.SeekCircle;
+
+public class SampleActivity extends Activity
+{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		SeekCircle seekCircle = (SeekCircle)findViewById(R.id.seekCircle);
+		seekCircle.setOnSeekCircleChangeListener(new SeekCircle.OnSeekCircleChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekCircle seekCircle)
+			{}
+			
+			@Override
+			public void onStartTrackingTouch(SeekCircle seekCircle)
+			{}
+			
+			@Override
+			public void onProgressChanged(SeekCircle seekCircle, int progress, boolean fromUser)
+			{
+				updateText();
+			}
+		});
+
+		updateText();
+	}
+	
+	private void updateText()
+	{
+		SeekCircle seekCircle = (SeekCircle)findViewById(R.id.seekCircle);
+		TextView textProgress = (TextView)findViewById(R.id.textProgress);
+
+		if (textProgress != null && seekCircle != null)
+		{
+			int progress = seekCircle.getProgress();
+			textProgress.setText(Integer.toString(progress) + "%");
+		}
+	}
+}
