a3c15b7e848a7d2627336633d6b182936bdacc6c
Necat0r <j_lindmark@hotmail.com>
2016-01-11 05:09:21 +0100

    Reformatted code with Android Studio
    Tab->space causing large diffs

--- library/src/com/seekcircle/ProgressCircle.java
+++ library/src/com/seekcircle/ProgressCircle.java
@@ -11,277 +11,253 @@ import android.view.View;
 
 import com.seekcircle.library.R;
 
-public class ProgressCircle extends View
-{
-	private float mRingBias = 0.15f;
-	private float mSectionRatio = 5.0f;
-	private RectF mSectionRect = new RectF();
-	protected float mSectionHeight;
-	
-	protected float mRadius;
-	
-	protected int mMinProgress = 0;
-	protected int mMaxProgress = 100;
-	protected int mProgress = 0;
-	
-	protected float mCenterX;
-	protected float mCenterY;
-	
-	private Paint mPaint = new Paint();
-	private int mColor1 = Color.parseColor("#ff33b5e5");
-	private int mColor2 = Color.parseColor("#ffff5900");
-	private int mInactiveColor = Color.parseColor("#ff404040");
-	
-	{
-		mPaint.setAntiAlias(true);
-		mPaint.setStyle(Paint.Style.FILL);
-	}
-	
-	public ProgressCircle(Context context, AttributeSet attrs, int defStyle)
-	{
-		super(context, attrs, defStyle);
-		
-		initAttributes(context, attrs);
-	}
-	
-	public ProgressCircle(Context context, AttributeSet attrs)
-	{
-		super(context, attrs);
-		
-		initAttributes(context, attrs);
-	}
-	
-	public ProgressCircle(Context context)
-	{
-		super(context);
-	}
-	
-	private void initAttributes(Context context, AttributeSet attrs)
-	{
-		TypedArray attributes = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SeekCircle, 0, 0);
-		try
-		{
+public class ProgressCircle extends View {
+    private float mRingBias = 0.15f;
+    private float mSectionRatio = 5.0f;
+    private RectF mSectionRect = new RectF();
+    protected float mSectionHeight;
+
+    protected float mRadius;
+
+    protected int mMinProgress = 0;
+    protected int mMaxProgress = 100;
+    protected int mProgress = 0;
+
+    protected float mCenterX;
+    protected float mCenterY;
+
+    private Paint mPaint = new Paint();
+    private int mColor1 = Color.parseColor("#ff33b5e5");
+    private int mColor2 = Color.parseColor("#ffff5900");
+    private int mInactiveColor = Color.parseColor("#ff404040");
+
+    {
+        mPaint.setAntiAlias(true);
+        mPaint.setStyle(Paint.Style.FILL);
+    }
+
+    public ProgressCircle(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        initAttributes(context, attrs);
+    }
+
+    public ProgressCircle(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        initAttributes(context, attrs);
+    }
+
+    public ProgressCircle(Context context) {
+        super(context);
+    }
+
+    private void initAttributes(Context context, AttributeSet attrs) {
+        TypedArray attributes = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SeekCircle, 0, 0);
+        try {
             // Read and clamp min
             int min = attributes.getInteger(R.styleable.SeekCircle_min, 0);
             mMinProgress = Math.max(min, 0);
 
             // Read and clamp max
-			int max = attributes.getInteger(R.styleable.SeekCircle_max, 100);
-			mMaxProgress = Math.max(max, 1); 
-			
-			// Read and clamp progress
-			int progress = attributes.getInteger(R.styleable.SeekCircle_progress, 0);
-			mProgress = Math.max(Math.min(progress, mMaxProgress), 0);
-			
-			mColor1 = attributes.getColor(R.styleable.SeekCircle_startColor, Color.parseColor("#ff33b5e5"));
-			mColor2 = attributes.getColor(R.styleable.SeekCircle_endColor, Color.parseColor("#ffff5900"));
-			mInactiveColor = attributes.getColor(R.styleable.SeekCircle_inactiveColor, Color.parseColor("#ff404040"));
-			
-			mRingBias = attributes.getFloat(R.styleable.SeekCircle_ringBias, 0.15f);
-			mSectionRatio = attributes.getFloat(R.styleable.SeekCircle_sectionRatio, 5.0f);
-		}
-		finally
-		{
-			attributes.recycle();
-		}
-	}
-	
-	private void updateDimensions(int width, int height)
-	{
-		// Update center position
-		mCenterX = width / 2.0f;
-		mCenterY = height / 2.0f;
-		
-		// Find shortest dimension
-		int diameter = Math.min(width, height);
-		
-		float outerRadius = diameter / 2;
-		float sectionHeight = outerRadius * mRingBias;
-		float sectionWidth = sectionHeight / mSectionRatio;
-		
-		mRadius = outerRadius - sectionHeight / 2;
-		mSectionRect.set(-sectionWidth / 2, -sectionHeight / 2, sectionWidth / 2, sectionHeight / 2);
-		mSectionHeight = sectionHeight;
-	}
-	
-	@Override
-	protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
-	{
-		int width = MeasureSpec.getSize(widthMeasureSpec);
-		int height = MeasureSpec.getSize(heightMeasureSpec);
-		
-		if (width > height)
-			super.onMeasure(heightMeasureSpec, widthMeasureSpec);
-		else
-			super.onMeasure(widthMeasureSpec, widthMeasureSpec);
-		
-		updateDimensions(getWidth(), getHeight());
-	}
-	
-	@Override
-	protected void onSizeChanged(int w, int h, int oldw, int oldh)
-	{
-		super.onSizeChanged(w, h, oldw, oldh);
-		
-		updateDimensions(w, h);
-	}
-	
-	@Override
-	protected void onDraw(Canvas canvas)
-	{
-		// Center our canvas
-		canvas.translate(mCenterX, mCenterY);
+            int max = attributes.getInteger(R.styleable.SeekCircle_max, 100);
+            mMaxProgress = Math.max(max, 1);
 
-		int relativeProgress = mProgress - mMinProgress;
-		int relativeMax = mMaxProgress - mMinProgress;
-		float rotation = 360.0f / (float) relativeMax;
-		for (int i = 0; i < relativeMax; ++i)
-		{
-			canvas.save();
-			
-			canvas.rotate((float) i * rotation);
-			canvas.translate(0, -mRadius);
+            // Read and clamp progress
+            int progress = attributes.getInteger(R.styleable.SeekCircle_progress, 0);
+            mProgress = Math.max(Math.min(progress, mMaxProgress), 0);
 
-			if (i < relativeProgress)
-			{
-				float bias = (float) i / (float) (relativeMax - 1);
-				int color = interpolateColor(mColor1, mColor2, bias);
-				mPaint.setColor(color);
-			}
-			else
-			{
-				canvas.scale(0.7f, 0.7f);
-				mPaint.setColor(mInactiveColor);
-			}
-			
-			canvas.drawRect(mSectionRect, mPaint);
-			canvas.restore();
-		}
-		
-		super.onDraw(canvas);
-	}
-	
-	private float interpolate(float a, float b, float bias)
-	{
-		return (a + ((b - a) * bias));
-	}
+            mColor1 = attributes.getColor(R.styleable.SeekCircle_startColor, Color.parseColor("#ff33b5e5"));
+            mColor2 = attributes.getColor(R.styleable.SeekCircle_endColor, Color.parseColor("#ffff5900"));
+            mInactiveColor = attributes.getColor(R.styleable.SeekCircle_inactiveColor, Color.parseColor("#ff404040"));
 
-	private int interpolateColor(int colorA, int colorB, float bias)
-	{
-		float[] hsvColorA = new float[3];
-		Color.colorToHSV(colorA, hsvColorA);
+            mRingBias = attributes.getFloat(R.styleable.SeekCircle_ringBias, 0.15f);
+            mSectionRatio = attributes.getFloat(R.styleable.SeekCircle_sectionRatio, 5.0f);
+        } finally {
+            attributes.recycle();
+        }
+    }
 
-		float[] hsvColorB = new float[3];
-		Color.colorToHSV(colorB, hsvColorB);
+    private void updateDimensions(int width, int height) {
+        // Update center position
+        mCenterX = width / 2.0f;
+        mCenterY = height / 2.0f;
 
-		hsvColorB[0] = interpolate(hsvColorA[0], hsvColorB[0], bias);
-		hsvColorB[1] = interpolate(hsvColorA[1], hsvColorB[1], bias);
-		hsvColorB[2] = interpolate(hsvColorA[2], hsvColorB[2], bias);
-		
-		// NOTE For some reason the method HSVToColor fail in edit mode. Just use the start color for now
-		if (isInEditMode())
-			return colorA;
+        // Find shortest dimension
+        int diameter = Math.min(width, height);
 
-		return Color.HSVToColor(hsvColorB);
-	}
+        float outerRadius = diameter / 2;
+        float sectionHeight = outerRadius * mRingBias;
+        float sectionWidth = sectionHeight / mSectionRatio;
 
-	/**
-	 * Get min progress
-	 *
-	 * @return Min progress
-	 */
-	public float getMin() { return mMinProgress; }
+        mRadius = outerRadius - sectionHeight / 2;
+        mSectionRect.set(-sectionWidth / 2, -sectionHeight / 2, sectionWidth / 2, sectionHeight / 2);
+        mSectionHeight = sectionHeight;
+    }
 
-	/**
-	 * Set min progress
-	 *
-	 * @param min
-	 */
-	public void setMin(int min)
-	{
-		int newMin = Math.max(0, min);
-		if (newMin != mMinProgress)
-			mMinProgress = newMin;
+    @Override
+    protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
 
-		updateProgress(mProgress);
-		invalidate();
-	}
+        if (width > height)
+            super.onMeasure(heightMeasureSpec, widthMeasureSpec);
+        else
+            super.onMeasure(widthMeasureSpec, widthMeasureSpec);
 
-	/**
-	 * Get max progress
-	 * 
-	 * @return Max progress
-	 */
-	public float getMax()
-	{
-		return mMaxProgress;
-	}
-	
-	/**
-	 * Set max progress
-	 * 
-	 * @param max
-	 */
-	public void setMax(int max)
-	{
-		int newMax = Math.max(max, 1);
-		if (newMax != mMaxProgress)
-			mMaxProgress = newMax;
-		
-		updateProgress(mProgress);
-		invalidate();
-	}
-	
-	/**
-	 * Get Progress
-	 * 
-	 * @return progress
-	 */
-	public int getProgress()
-	{
-		return mProgress;
-	}
-	
-	/**
-	 * Set progress
-	 * 
-	 * @param progress
-	 */
-	public void setProgress(int progress)
-	{
-		updateProgress(progress);
-	}
-	
-	/**
-	 * Get ratio
-	 * 
-	 * @return progress ratio compared to max progress 
-	 */
-	public float getRatio()
-	{
-		int relativeMax = mMaxProgress - mMinProgress;
-		return (float)(mProgress - mMinProgress) / (float)relativeMax;
-	}
-	
-	/**
-	 * Update progress internally. Clamp it to a valid range and invalidate the view if necessary 
-	 * 
-	 * @param progress
-	 * @return true if progress was changed and the view needs an update
-	 */
-	protected boolean updateProgress(int progress)
-	{
-		// Clamp progress
-		progress = Math.max(mMinProgress, Math.min(mMaxProgress, progress));
+        updateDimensions(getWidth(), getHeight());
+    }
 
-		if (progress != mProgress)
-		{
-			mProgress = progress;
-			invalidate();
-			
-			return true;
-		}
-		
-		return false;
-	}
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        updateDimensions(w, h);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // Center our canvas
+        canvas.translate(mCenterX, mCenterY);
+
+        int relativeProgress = mProgress - mMinProgress;
+        int relativeMax = mMaxProgress - mMinProgress;
+        float rotation = 360.0f / (float) relativeMax;
+        for (int i = 0; i < relativeMax; ++i) {
+            canvas.save();
+
+            canvas.rotate((float) i * rotation);
+            canvas.translate(0, -mRadius);
+
+            if (i < relativeProgress) {
+                float bias = (float) i / (float) (relativeMax - 1);
+                int color = interpolateColor(mColor1, mColor2, bias);
+                mPaint.setColor(color);
+            } else {
+                canvas.scale(0.7f, 0.7f);
+                mPaint.setColor(mInactiveColor);
+            }
+
+            canvas.drawRect(mSectionRect, mPaint);
+            canvas.restore();
+        }
+
+        super.onDraw(canvas);
+    }
+
+    private float interpolate(float a, float b, float bias) {
+        return (a + ((b - a) * bias));
+    }
+
+    private int interpolateColor(int colorA, int colorB, float bias) {
+        float[] hsvColorA = new float[3];
+        Color.colorToHSV(colorA, hsvColorA);
+
+        float[] hsvColorB = new float[3];
+        Color.colorToHSV(colorB, hsvColorB);
+
+        hsvColorB[0] = interpolate(hsvColorA[0], hsvColorB[0], bias);
+        hsvColorB[1] = interpolate(hsvColorA[1], hsvColorB[1], bias);
+        hsvColorB[2] = interpolate(hsvColorA[2], hsvColorB[2], bias);
+
+        // NOTE For some reason the method HSVToColor fail in edit mode. Just use the start color for now
+        if (isInEditMode())
+            return colorA;
+
+        return Color.HSVToColor(hsvColorB);
+    }
+
+    /**
+     * Get min progress
+     *
+     * @return Min progress
+     */
+    public float getMin() {
+        return mMinProgress;
+    }
+
+    /**
+     * Set min progress
+     *
+     * @param min
+     */
+    public void setMin(int min) {
+        int newMin = Math.max(0, min);
+        if (newMin != mMinProgress)
+            mMinProgress = newMin;
+
+        updateProgress(mProgress);
+        invalidate();
+    }
+
+    /**
+     * Get max progress
+     *
+     * @return Max progress
+     */
+    public float getMax() {
+        return mMaxProgress;
+    }
+
+    /**
+     * Set max progress
+     *
+     * @param max
+     */
+    public void setMax(int max) {
+        int newMax = Math.max(max, 1);
+        if (newMax != mMaxProgress)
+            mMaxProgress = newMax;
+
+        updateProgress(mProgress);
+        invalidate();
+    }
+
+    /**
+     * Get Progress
+     *
+     * @return progress
+     */
+    public int getProgress() {
+        return mProgress;
+    }
+
+    /**
+     * Set progress
+     *
+     * @param progress
+     */
+    public void setProgress(int progress) {
+        updateProgress(progress);
+    }
+
+    /**
+     * Get ratio
+     *
+     * @return progress ratio compared to max progress
+     */
+    public float getRatio() {
+        int relativeMax = mMaxProgress - mMinProgress;
+        return (float) (mProgress - mMinProgress) / (float) relativeMax;
+    }
+
+    /**
+     * Update progress internally. Clamp it to a valid range and invalidate the view if necessary
+     *
+     * @param progress
+     * @return true if progress was changed and the view needs an update
+     */
+    protected boolean updateProgress(int progress) {
+        // Clamp progress
+        progress = Math.max(mMinProgress, Math.min(mMaxProgress, progress));
+
+        if (progress != mProgress) {
+            mProgress = progress;
+            invalidate();
+
+            return true;
+        }
+
+        return false;
+    }
 }
--- library/src/com/seekcircle/SeekCircle.java
+++ library/src/com/seekcircle/SeekCircle.java
@@ -4,221 +4,202 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
-public class SeekCircle extends ProgressCircle
-{
-	/**
-	 * A callback that notifies clients when the progress level has been
-	 * changed. This includes changes that were initiated by the user through a
-	 * touch gesture or arrow key/trackball as well as changes that were
-	 * initiated programmatically.
-	 */
-	public interface OnSeekCircleChangeListener
-	{
-		
-		/**
-		 * Notification that the progress level has changed. Clients can use the
-		 * fromUser parameter to distinguish user-initiated changes from those
-		 * that occurred programmatically.
-		 * 
-		 * @param seekCircle
-		 *            The SeekCircle whose progress has changed
-		 * @param progress
-		 *            The current progress level. This will be in the range
-		 *            0..max where max was set by
-		 *            {@link ProgressCircle#setMax(int)}. (The default value for
-		 *            max is 100.)
-		 * @param fromUser
-		 *            True if the progress change was initiated by the user.
-		 */
-		void onProgressChanged(SeekCircle seekCircle, int progress, boolean fromUser);
-		
-		/**
-		 * Notification that the user has started a touch gesture. Clients may
-		 * want to use this to disable advancing the seek circle.
-		 * 
-		 * @param seekCircle
-		 *            The SeekCircle in which the touch gesture began
-		 */
-		void onStartTrackingTouch(SeekCircle seekCircle);
-		
-		/**
-		 * Notification that the user has finished a touch gesture. Clients may
-		 * want to use this to re-enable advancing the seek circle.
-		 * 
-		 * @param seekCircle
-		 *            The SeekCircle in which the touch gesture began
-		 */
-		void onStopTrackingTouch(SeekCircle seekCircle);
-	}
-	
-	private OnSeekCircleChangeListener mOnSeekCircleChangeListener;
-	
-	/**
-	 * Sets a listener to receive notifications of changes to the SeekCircle's
-	 * progress level. Also provides notifications of when the user starts and
-	 * stops a touch gesture within the SeekCircle.
-	 * 
-	 * @param listener
-	 *            The seek circle notification listener
-	 * 
-	 * @see SeekCircle.OnSeekCircleChangeListener
-	 */
-	public void setOnSeekCircleChangeListener(OnSeekCircleChangeListener listener)
-	{
-		mOnSeekCircleChangeListener = listener;
-	}
-	
-	private boolean mTrackingTouch = false;
-	private int mRevolutions = 0;
-	private float mOldX;
-	private boolean mEnabled = true;
-	
-	public SeekCircle(Context context, AttributeSet attrs, int defStyle)
-	{
-		super(context, attrs, defStyle);
-	}
-	
-	public SeekCircle(Context context, AttributeSet attrs)
-	{
-		super(context, attrs);
-	}
-	
-	public SeekCircle(Context context)
-	{
-		super(context);
-	}
+public class SeekCircle extends ProgressCircle {
+    /**
+     * A callback that notifies clients when the progress level has been
+     * changed. This includes changes that were initiated by the user through a
+     * touch gesture or arrow key/trackball as well as changes that were
+     * initiated programmatically.
+     */
+    public interface OnSeekCircleChangeListener {
 
-	public void setEnabled(boolean enabled) {
-		mEnabled = enabled;
-	}
+        /**
+         * Notification that the progress level has changed. Clients can use the
+         * fromUser parameter to distinguish user-initiated changes from those
+         * that occurred programmatically.
+         *
+         * @param seekCircle The SeekCircle whose progress has changed
+         * @param progress   The current progress level. This will be in the range
+         *                   0..max where max was set by
+         *                   {@link ProgressCircle#setMax(int)}. (The default value for
+         *                   max is 100.)
+         * @param fromUser   True if the progress change was initiated by the user.
+         */
+        void onProgressChanged(SeekCircle seekCircle, int progress, boolean fromUser);
 
-	public boolean isEnabled() {
-		return mEnabled;
-	}
+        /**
+         * Notification that the user has started a touch gesture. Clients may
+         * want to use this to disable advancing the seek circle.
+         *
+         * @param seekCircle The SeekCircle in which the touch gesture began
+         */
+        void onStartTrackingTouch(SeekCircle seekCircle);
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event)
-	{
-		// TODO Handle return value better.
-		// TODO Moving outside view doesn't cancel notification nor progress updates
+        /**
+         * Notification that the user has finished a touch gesture. Clients may
+         * want to use this to re-enable advancing the seek circle.
+         *
+         * @param seekCircle The SeekCircle in which the touch gesture began
+         */
+        void onStopTrackingTouch(SeekCircle seekCircle);
+    }
 
-		if (!mEnabled) { return false; }
+    private OnSeekCircleChangeListener mOnSeekCircleChangeListener;
 
-		// Right hand coordinates X to the right, Y up
-		float x = event.getX() - mCenterX;
-		float y = mCenterY - event.getY();
+    /**
+     * Sets a listener to receive notifications of changes to the SeekCircle's
+     * progress level. Also provides notifications of when the user starts and
+     * stops a touch gesture within the SeekCircle.
+     *
+     * @param listener The seek circle notification listener
+     * @see SeekCircle.OnSeekCircleChangeListener
+     */
+    public void setOnSeekCircleChangeListener(OnSeekCircleChangeListener listener) {
+        mOnSeekCircleChangeListener = listener;
+    }
 
-		float distance = (float) Math.sqrt(x * x + y * y);
+    private boolean mTrackingTouch = false;
+    private int mRevolutions = 0;
+    private float mOldX;
+    private boolean mEnabled = true;
 
-		boolean inRange = Math.abs(distance - mRadius) <= mSectionHeight;
+    public SeekCircle(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
 
-		boolean updateProgress = false;
+    public SeekCircle(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-		switch (event.getAction()) {
-			case MotionEvent.ACTION_DOWN:
-				if (inRange) {
-					mTrackingTouch = true;
-					mOldX = x;
-					mRevolutions = 0;
-					updateProgress = true;
-				}
+    public SeekCircle(Context context) {
+        super(context);
+    }
 
-				break;
+    public void setEnabled(boolean enabled) {
+        mEnabled = enabled;
+    }
 
-			case MotionEvent.ACTION_MOVE:
-				if (mTrackingTouch)
-					updateProgress = true;
-				break;
+    public boolean isEnabled() {
+        return mEnabled;
+    }
 
-			case MotionEvent.ACTION_UP:
-				if (mTrackingTouch)
-					updateProgress = true;
-				mTrackingTouch = false;
-				break;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // TODO Handle return value better.
+        // TODO Moving outside view doesn't cancel notification nor progress updates
 
-			case MotionEvent.ACTION_CANCEL:
-				mTrackingTouch = false;
-				break;
-		}
+        if (!mEnabled) {
+            return false;
+        }
 
-		int relativeMax = mMaxProgress - mMinProgress;
+        // Right hand coordinates X to the right, Y up
+        float x = event.getX() - mCenterX;
+        float y = mCenterY - event.getY();
 
-		if (updateProgress) {
-			// Calculate absolute position [0, 1] with 0 & 1 both at 12-o-clock
-			float position = (float) ((Math.atan2(-x, -y) + Math.PI) / (Math.PI * 2.0));
-			int relativeProgress = Math.round(position * ((float) relativeMax));
+        float distance = (float) Math.sqrt(x * x + y * y);
 
-			if (event.getAction() != MotionEvent.ACTION_DOWN) {
-				updateRevolutions(x, y);
+        boolean inRange = Math.abs(distance - mRadius) <= mSectionHeight;
 
-				float absPosition = (float) mRevolutions + position;
+        boolean updateProgress = false;
 
-				// Clamp progress
-				if (absPosition < 0.0f)
-					relativeProgress = 0;
-				else if (absPosition > 1.0f)
-					relativeProgress = relativeMax;
-			}
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (inRange) {
+                    mTrackingTouch = true;
+                    mOldX = x;
+                    mRevolutions = 0;
+                    updateProgress = true;
+                }
 
-			mOldX = x;
-			updateTouchProgress(mMinProgress + relativeProgress);
+                break;
 
-			if (event.getAction() == MotionEvent.ACTION_DOWN && mOnSeekCircleChangeListener != null) {
-				mOnSeekCircleChangeListener.onStartTrackingTouch(this);
-			} else if (event.getAction() == MotionEvent.ACTION_UP && mOnSeekCircleChangeListener != null) {
-				mOnSeekCircleChangeListener.onStopTrackingTouch(this);
-			}
+            case MotionEvent.ACTION_MOVE:
+                if (mTrackingTouch)
+                    updateProgress = true;
+                break;
 
-			return true; //super.onTouchEvent(event);
-		}
+            case MotionEvent.ACTION_UP:
+                if (mTrackingTouch)
+                    updateProgress = true;
+                mTrackingTouch = false;
+                break;
 
-		return super.onTouchEvent(event);
-	}
-	
-	/**
-	 * Update the number of revolutions we're at
-	 * 
-	 * @param x
-	 *            X position
-	 * @param y
-	 *            Y position
-	 */
-	private void updateRevolutions(float x, float y)
-	{
-		// We're in the upper half and X just flipped
-		boolean leftFlip = y > 0.0f && mOldX >= 0.0f && x < 0.0f;
-		boolean rightFlip = y > 0.0f && mOldX <= 0.0f && x > 0.0f;
-		
-		if (leftFlip)
-			mRevolutions -= 1.0f;
-		else if (rightFlip)
-			mRevolutions += 1.0f;
-		
-		// Clamp windings to [-1, 1]
-		mRevolutions = Math.max(-1, Math.min(1, mRevolutions));
-	}
-	
-	@Override
-	protected boolean updateProgress(int progress)
-	{
-		boolean result = super.updateProgress(progress);
-		if (result)
-		{
-			// Reset position to match
-			if (mOnSeekCircleChangeListener != null)
-				mOnSeekCircleChangeListener.onProgressChanged(this, progress, false);
-		}
-		
-		return result;
-	}
-	
-	private void updateTouchProgress(int progress)
-	{
-		boolean result = super.updateProgress(progress);
-		if (result)
-		{
-			if (mOnSeekCircleChangeListener != null)
-				mOnSeekCircleChangeListener.onProgressChanged(this, progress, true);
-		}
-	}
+            case MotionEvent.ACTION_CANCEL:
+                mTrackingTouch = false;
+                break;
+        }
+
+        int relativeMax = mMaxProgress - mMinProgress;
+
+        if (updateProgress) {
+            // Calculate absolute position [0, 1] with 0 & 1 both at 12-o-clock
+            float position = (float) ((Math.atan2(-x, -y) + Math.PI) / (Math.PI * 2.0));
+            int relativeProgress = Math.round(position * ((float) relativeMax));
+
+            if (event.getAction() != MotionEvent.ACTION_DOWN) {
+                updateRevolutions(x, y);
+
+                float absPosition = (float) mRevolutions + position;
+
+                // Clamp progress
+                if (absPosition < 0.0f)
+                    relativeProgress = 0;
+                else if (absPosition > 1.0f)
+                    relativeProgress = relativeMax;
+            }
+
+            mOldX = x;
+            updateTouchProgress(mMinProgress + relativeProgress);
+
+            if (event.getAction() == MotionEvent.ACTION_DOWN && mOnSeekCircleChangeListener != null) {
+                mOnSeekCircleChangeListener.onStartTrackingTouch(this);
+            } else if (event.getAction() == MotionEvent.ACTION_UP && mOnSeekCircleChangeListener != null) {
+                mOnSeekCircleChangeListener.onStopTrackingTouch(this);
+            }
+
+            return true; //super.onTouchEvent(event);
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    /**
+     * Update the number of revolutions we're at
+     *
+     * @param x X position
+     * @param y Y position
+     */
+    private void updateRevolutions(float x, float y) {
+        // We're in the upper half and X just flipped
+        boolean leftFlip = y > 0.0f && mOldX >= 0.0f && x < 0.0f;
+        boolean rightFlip = y > 0.0f && mOldX <= 0.0f && x > 0.0f;
+
+        if (leftFlip)
+            mRevolutions -= 1.0f;
+        else if (rightFlip)
+            mRevolutions += 1.0f;
+
+        // Clamp windings to [-1, 1]
+        mRevolutions = Math.max(-1, Math.min(1, mRevolutions));
+    }
+
+    @Override
+    protected boolean updateProgress(int progress) {
+        boolean result = super.updateProgress(progress);
+        if (result) {
+            // Reset position to match
+            if (mOnSeekCircleChangeListener != null)
+                mOnSeekCircleChangeListener.onProgressChanged(this, progress, false);
+        }
+
+        return result;
+    }
+
+    private void updateTouchProgress(int progress) {
+        boolean result = super.updateProgress(progress);
+        if (result) {
+            if (mOnSeekCircleChangeListener != null)
+                mOnSeekCircleChangeListener.onProgressChanged(this, progress, true);
+        }
+    }
 }
